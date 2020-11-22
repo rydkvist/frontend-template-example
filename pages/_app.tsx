21a340d88a67ff6306578a477eef8d1713aa488d
Nojze <niklasryd01@gmail.com>
2020-11-22 16:19:24 +0100

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,11 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </Head>
       <GlobalStyle />
       <Header />
