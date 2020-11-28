a75ab2f8b4aca0b3a4e46a2d9f2a983ae34ec471
Nojze <niklasryd01@gmail.com>
2020-11-28 13:25:56 +0100

    fix imports

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import { appName } from 'config';
 import { Page } from '@styles';
 import { Button } from '@components';
