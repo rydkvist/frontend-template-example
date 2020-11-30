1e4dedb965ae8e960a632c7c57103b1cbe0c95c3
Nojze <niklasryd01@gmail.com>
2020-11-30 09:08:54 +0100

    Add buttons to all pages

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,19 +1,31 @@
 import Head from 'next/head';
 import { appName } from 'config';
 import { Page } from '@styles';
+import { Button } from '@components';
 
-const About = () => (
-  <>
-    <Head>
-      <title>{appName}: About</title>
-    </Head>
-    <Page>
-      <h1 style={{ padding: 0, margin: 0 }}>
-        <ion-icon name="information-circle-outline" />
-      </h1>
-      <h1 className="bg-blue-200">About</h1>
-    </Page>
-  </>
-);
+const About = () => {
+  const onClick = () => {
+    alert('Try tabbing on me instead of clicking me! :)');
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{appName}: About</title>
+      </Head>
+      <Page>
+        <h1 style={{ padding: 0, margin: 0 }}>
+          <ion-icon name="information-circle-outline" />
+        </h1>
+        <h1 className="bg-blue-200">About</h1>
+        <Button onClick={onClick}>Button 1</Button>
+        <Button onClick={onClick}>Button 2</Button>
+        <Button onClick={onClick}>Button 3</Button>
+        <Button onClick={onClick}>Button 4</Button>
+        <Button onClick={onClick}>Button 5</Button>
+      </Page>
+    </>
+  );
+};
 
 export default About;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,19 +1,31 @@
 import Head from 'next/head';
 import { appName } from 'config';
 import { Page } from '@styles';
+import { Button } from '@components';
 
-const Dashboard = () => (
-  <>
-    <Head>
-      <title>{appName}: Dashboard</title>
-    </Head>
-    <Page>
-      <h1 style={{ padding: 0, margin: 0 }}>
-        <ion-icon name="grid-outline" />
-      </h1>
-      <h1>Dashboard</h1>
-    </Page>
-  </>
-);
+const Dashboard = () => {
+  const onClick = () => {
+    alert('Try tabbing on me instead of clicking me! :)');
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{appName}: Dashboard</title>
+      </Head>
+      <Page>
+        <h1 style={{ padding: 0, margin: 0 }}>
+          <ion-icon name="grid-outline" />
+        </h1>
+        <h1>Dashboard</h1>
+        <Button onClick={onClick}>Button 1</Button>
+        <Button onClick={onClick}>Button 2</Button>
+        <Button onClick={onClick}>Button 3</Button>
+        <Button onClick={onClick}>Button 4</Button>
+        <Button onClick={onClick}>Button 5</Button>
+      </Page>
+    </>
+  );
+};
 
 export default Dashboard;
